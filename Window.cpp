5e212845b315a6e41ee56d0d34eb97d325a19d2f
vick-genis <99092155+vick-genis@users.noreply.github.com>
2024-05-06 14:47:10 -0600

    Add files via upload

--- Window.cpp
+++ Window.cpp
@@ -1,4 +1,12 @@
 #include "Window.h"
+#include <irrKlang/irrKlang.h>
+using namespace irrklang;
+#include <al.h>
+#include <alc.h>
+#include <conio.h>
+#include <thread> // Para this_thread
+#include <chrono> // Para chrono
+#include "CameraManager.h"
 
 Window::Window()
 {
@@ -16,23 +24,26 @@ Window::Window(GLint windowWidth, GLint windowHeight)
 	rotax = 0.0f;
 	rotay = 0.0f;
 	rotaz = 0.0f;
+	muevex = 2.0f;
+	prendeluzspot = 1.0f;
+	cambialuzspot = 1.0f;
+	prendeluzspot2 = 1.0f;
+	cambialuzspot2 = 1.0f;
+	prendeluzspot3 = 1.0f;
+	//cambialuzfaros = 2.0f;
+	for (size_t i = 0; i < 1024; i++)
+	{
+		keys[i] = 0;
+	}
 	articulacion1 = 0.0f;
 	articulacion2 = 0.0f;
 	articulacion3 = 0.0f;
 	articulacion4 = 0.0f;
 	articulacion5 = 0.0f;
 	articulacion6 = 0.0f;
-	articulacion7 = 0.0f;
-	articulacion8 = 0.0f;
-	articulacion9 = 0.0f;
-	articulacion10 = 0.0f;
-	articulacion11 = 0.0f;
-	articulacion12 = 0.0f;
-	
-	for (size_t i = 0; i < 1024; i++)
-	{
-		keys[i] = 0;
-	}
+	positionX = 2.0f;
+	positionZ = 2.0f;
+	//valorCamara = 0.0f;
 }
 int Window::Initialise()
 {
@@ -51,7 +62,7 @@ int Window::Initialise()
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	//CREAR VENTANA
-	mainWindow = glfwCreateWindow(width, height, "Practica XX: Nombre de la práctica", NULL, NULL);
+	mainWindow = glfwCreateWindow(width, height, "Proyecto", NULL, NULL);
 
 	if (!mainWindow)
 	{
@@ -87,6 +98,32 @@ int Window::Initialise()
 	glViewport(0, 0, bufferWidth, bufferHeight);
 	//Callback para detectar que se está usando la ventana
 	glfwSetWindowUserPointer(mainWindow, this);
+	// Configurar punteros de usuario
+	//glfwSetWindowUserPointer(mainWindow, &cameraManager);
+	
+	// Crear el motor de sonido (PISTA DE AUDIO CONTINUA)
+	ISoundEngine* engine = createIrrKlangDevice();
+
+	// Verificar si se creó correctamente el motor de sonido
+	if (!engine) {
+		printf("No se pudo crear el motor de sonido\n");
+		return 1;
+	}
+
+	// Reproducir un archivo de música
+	ISound* sound = engine->play3D("D:/COMPU GRAFICA/Proyecto/Proyecto/Proyecto/media/This Is Halloween 8-bit.mp3",
+		vec3df(0, 0, 0), true, false, true);
+	// Verificar si se pudo iniciar la reproducción
+	if (!sound) {
+		printf("Error al reproducir el archivo de musica\n");
+		engine->drop(); // Liberar el motor de sonido
+		return 1;
+	}
+	// Bajar el volumen 
+	sound->setVolume(0.1f);
+	//Creando motor de audio para que permita audio 3d (posicional)
+	//D:/COMPU GRAFICA/Proyecto/Proyecto/Proyecto/media/Cicada.wav
+	
 }
 
 void Window::createCallbacks()
@@ -94,7 +131,6 @@ void Window::createCallbacks()
 	glfwSetKeyCallback(mainWindow, ManejaTeclado);
 	glfwSetCursorPosCallback(mainWindow, ManejaMouse);
 }
-
 GLfloat Window::getXChange()
 {
 	GLfloat theChange = xChange;
@@ -109,83 +145,198 @@ GLfloat Window::getYChange()
 	return theChange;
 }
 
+
+
+
 void Window::ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode)
 {
 	Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+	//CameraManager* theCamera = static_cast<CameraManager*>(glfwGetWindowUserPointer(window));
+
 
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 	{
 		glfwSetWindowShouldClose(window, GL_TRUE);
 	}
-
-	
-	if (key == GLFW_KEY_E)
+	if (key == GLFW_KEY_Y)
 	{
-		theWindow->rotax += 10.0;
+		theWindow-> muevex += 1.0;
 	}
-	if (key == GLFW_KEY_R)
+	if (key == GLFW_KEY_U)
 	{
-		theWindow->rotay += 10.0; //rotar sobre el eje y 10 grados
-	}
-	if (key == GLFW_KEY_T)
-	{
-		theWindow->rotaz += 10.0;
-	}
-	if (key == GLFW_KEY_F)
-	{
-		theWindow->articulacion1 += 10.0;
+		theWindow-> muevex -= 1.0;
 	}
 
-	if (key == GLFW_KEY_G)
+	if (key == GLFW_KEY_V)
 	{
-		theWindow->articulacion2 += 10.0;
+		if (theWindow->anguloCofre < -45.0)
+		{
+			// Por ahora no vamos a anadir nada aqui
+		}
+		else
+		{
+			theWindow->anguloCofre -= 10.0;
+		}
 	}
-	if (key == GLFW_KEY_H)
+	else if (key == GLFW_KEY_B) // Cambiado a "else if" para garantizar que solo se evalúe si no es GLFW_KEY_V
 	{
-		theWindow->articulacion3 += 10.0;
+		if (theWindow->anguloCofre > -1.0)
+		{
+			// Por ahora no vamos a anadir nada aqui
+		}
+		else
+		{
+			theWindow->anguloCofre += 10.0;
+		}
 	}
-	if (key == GLFW_KEY_J)
-	{
-		theWindow->articulacion4 += 10.0;
+
+	//Teclas
+	if (action == GLFW_PRESS) {
+		if (key == GLFW_KEY_K) {
+			theWindow->isKPressed = true;
+		}
+		else if (key == GLFW_KEY_L) {
+			theWindow->isLPressed = true;
+		}
 	}
-	if (key == GLFW_KEY_K)
-	{
-		theWindow->articulacion5 += 10.0;
+	else if (action == GLFW_RELEASE) {
+		if (key == GLFW_KEY_K) {
+			theWindow->isKPressed = false;
+		}
+		else if (key == GLFW_KEY_L) {
+			theWindow->isLPressed = false;
+		}
 	}
-	if (key == GLFW_KEY_L)
+
+	if (theWindow->isKPressed) {
+		theWindow->anguloLlantas += 10.0; // Girar hacia adelante
+		if (theWindow->anguloLlantas >= 360.0) {
+			theWindow->anguloLlantas -= 360.0; // Volver a 0 grados si se supera 360 grados
+		}
+	}
+	else if (theWindow->isLPressed) {
+		theWindow->anguloLlantas -= 10.0; // Girar hacia atrás
+		if (theWindow->anguloLlantas <= -360.0) {
+			theWindow->anguloLlantas += 360.0; // Volver a 0 grados si se supera -360 grados
+		}
+	}
+	//Movimientos para el carro 
+	if (key == GLFW_KEY_Z)
 	{
-		theWindow->articulacion6 += 10.0;
+		theWindow->positionX += 1.0;
+	}
+	if (key == GLFW_KEY_X)
+	{
+		theWindow->positionX -= 1.0;
+	}
+	if (key == GLFW_KEY_N)
+	{
+		theWindow->positionZ += 1.0;
+		theWindow->cambialuzfaros = true; // Activar el cambio de faros
+		printf("se presiono la tecla %d'\n", key);
+		//printf("el valor es %s'\n", theWindow->cambialuzfaros);
+		//printf("positionZ %f'\n", theWindow->positionZ);
+	}
+	if (key == GLFW_KEY_M)
+	{
+		theWindow->positionZ -= 1.0;
+		theWindow->cambialuzfaros = false; // Desactivar el cambio de faros
+		printf("se presiono la tecla %d'\n", key);
+		//printf("el valor es %s'\n", theWindow->cambialuzfaros);
+	}
+	//Movimientos para el helicoptero en X
+	if (key == GLFW_KEY_I)
+	{
+		theWindow->posX += 1.0;
 	}
 	if (key == GLFW_KEY_O)
 	{
-		theWindow->articulacion7 += 10.0;
+		theWindow->posX -= 1.0;
 	}
-	if (key == GLFW_KEY_P)
+	//Movimientos para el helicoptero en Y
+	if (key == GLFW_KEY_8)
 	{
-		theWindow->articulacion8 += 10.0;
+		theWindow->posY += 1.0;
 	}
-	if (key == GLFW_KEY_E)
+	if (key == GLFW_KEY_9)
 	{
-		theWindow->articulacion9 += 10.0;
+		theWindow->posY -= 1.0;
 	}
-	if (key == GLFW_KEY_R)
+	//tecla para aprender luces
+	if (key == GLFW_KEY_1 && action == GLFW_PRESS)
 	{
-		theWindow->articulacion10 += 10.0;
+		theWindow->cambialuzspot = -1.0f * theWindow->cambialuzspot;
 	}
-	if (key == GLFW_KEY_T)
+	//tecla para aprender luces de la lampara
+	if (key == GLFW_KEY_2 && action == GLFW_PRESS)
 	{
-		theWindow->articulacion11 += 10.0;
+		theWindow->cambialuzspot2 = -1.0f * theWindow->cambialuzspot2;
+		//printf("se presiono la tecla %d'\n", key);
 	}
-	if (key == GLFW_KEY_Y)
-	{
-		theWindow->articulacion12 += 10.0;
+	
+
+	/*
+	bool terceraPersonaActiva = true; // Inicialmente activa la tercera persona
+
+	// Dentro del bucle principal del juego o la función de manejo de eventos de teclado
+	if (key == GLFW_KEY_9 && action == GLFW_PRESS) {
+		printf("Se presiona la tecla 9\n");
+
+		// Cambiar el estado de la cámara
+		terceraPersonaActiva = !terceraPersonaActiva;
+
+		// Actualizar la cámara activa en el CameraManager según el nuevo estado
+		if (!terceraPersonaActiva) {
+			printf("Camara tercera persona activa\n");
+			theWindow->valorCamara = 1.0f; // Indicar que la cámara es la tercera persona
+			printf("el valor es %f'\n", theWindow->valorCamara);
+			//terceraPersonaActiva = !terceraPersonaActiva;
+		}
+		else {
+			printf("Camara aerea activa\n");
+			theWindow->valorCamara = 0.0f; // Indicar que la cámara es la aérea
+			printf("el valor es %f'\n", theWindow->valorCamara);
+			//terceraPersonaActiva = !terceraPersonaActiva;
+		}
+	}
+	*/
+	// Método para manejar la lógica cuando se presiona la tecla 9
+	
+	if (key == GLFW_KEY_9 && action == GLFW_PRESS) {
+		printf("Se presiona la tecla 9\n");
+		theWindow->valorCamara = 1.0f; // Devuelve 1 cuando se presiona la tecla 9
 	}
 
-	if (key == GLFW_KEY_D && action == GLFW_PRESS)
-	{
-		const char* key_name = glfwGetKeyName(GLFW_KEY_D, 0);
-		//printf("se presiono la tecla: %s\n",key_name);
+	// Método para manejar la lógica cuando se presiona la tecla 0
+	
+	if (key == GLFW_KEY_0 && action == GLFW_PRESS) {
+		printf("Se presiona la tecla 0\n");
+		theWindow->valorCamara = 0.0f; // Devuelve 0 cuando se presiona la tecla 0
 	}
+		
+
+
+
+
+
+
+	/*Camera thirdPersonCamera, aerialCamera;
+	CameraManager cameraManager;
+	if (key == GLFW_KEY_9 && action == GLFW_PRESS) {
+		cameraManager.switchCamera("terceraPersona");
+		printf("Se presiona la tecla 9\n");
+		theWindow->valorCamara = 1.0f;
+		printf("Camara tercera persona activa\n");
+		printf("ValorCamara %f'\n", theWindow->valorCamara);
+	}
+	else if (key == GLFW_KEY_0 && action == GLFW_PRESS) {
+		cameraManager.switchCamera("aerea");
+		printf("Se presiona la tecla 0\n");
+		theWindow->valorCamara = 0.0f;
+		printf("Camara aerea activa\n");
+		printf("ValorCamara %f'\n", theWindow->valorCamara);
+	}
+	*/
 
 	if (key >= 0 && key < 1024)
 	{
@@ -218,6 +369,8 @@ void Window::ManejaMouse(GLFWwindow* window, double xPos, double yPos)
 
 	theWindow->lastX = xPos;
 	theWindow->lastY = yPos;
+	//printf("X= %f Y=%f\n", theWindow->lastX, theWindow->lastY);
+
 }
 
 
--- Window.h
+++ Window.h
@@ -11,42 +11,68 @@ public:
 	int Initialise();
 	GLfloat getBufferWidth() { return bufferWidth; }
 	GLfloat getBufferHeight() { return bufferHeight; }
+	GLfloat getXChange();
+	GLfloat getYChange();
+	GLfloat getmuevex() { return muevex; }
 	bool getShouldClose() {
 		return  glfwWindowShouldClose(mainWindow);}
 	bool* getsKeys() { return keys; }
-	GLfloat getXChange();
-	GLfloat getYChange();
 	void swapBuffers() { return glfwSwapBuffers(mainWindow); }
-	GLfloat getrotay() { return rotay; }
-	GLfloat getrotax() { return rotax; }
-	GLfloat getrotaz() { return rotaz; }
-	GLfloat getarticulacion1() { return articulacion1; }
-	GLfloat getarticulacion2() { return articulacion2; }
-	GLfloat getarticulacion3() { return articulacion3; }
-	GLfloat getarticulacion4() { return articulacion4; }
-	GLfloat getarticulacion5() { return articulacion5; }
-	GLfloat getarticulacion6() { return articulacion6; }
-	GLfloat getarticulacion7() { return articulacion7; }
-	GLfloat getarticulacion8() { return articulacion8; }
-	GLfloat getarticulacion9() { return articulacion9; }
-	GLfloat getarticulacion10() { return articulacion10; }
-	GLfloat getarticulacion11() { return articulacion11; }
-	GLfloat getarticulacion12() { return articulacion12; }
-
+	GLfloat getanguloCofre() { return anguloCofre; }
+	GLfloat getanguloLlantas() { return anguloLlantas; }
+	GLfloat getmovimientoCocheZ() { return positionZ; }
+	GLfloat getmovimientoCocheX() { return positionX; }
+	GLfloat getmovimientoHelicopteroX() { return posX; }
+	GLfloat getmovimientoHelicopteroY() { return posY; }
+	GLfloat getprendeluzspot() { return prendeluzspot; }
+	GLfloat getcambialuzspot() { return cambialuzspot; }
+	GLfloat getprendeluzspot2() { return prendeluzspot2; }
+	GLfloat getcambialuzspot2() { return cambialuzspot2; }
+	GLfloat getprendeluzspot3() { return prendeluzspot3; }
+	GLfloat getcambiafaros() { return cambialuzfaros; }
+	//GLfloat getCambioCamara() { return valorCamara; }
+	GLfloat getCambioCamara1() { return valorCamara; }
+	GLfloat getCambioCamara2() { return valorCamara; }
+	GLfloat getMouseX() { return lastX; }
+	GLfloat getMouseY() { return lastY; }
+	GLfloat getMouseCoord() { return screenX, screenY,0.0f; }
 	~Window();
 private: 
 	GLFWwindow *mainWindow;
 	GLint width, height;
-	GLfloat rotax,rotay,rotaz, articulacion1, articulacion2, articulacion3, articulacion4, articulacion5, articulacion6, articulacion7, articulacion8, articulacion9, articulacion10, articulacion11,articulacion12;
 	bool keys[1024];
 	GLint bufferWidth, bufferHeight;
+	void createCallbacks();
 	GLfloat lastX;
 	GLfloat lastY;
 	GLfloat xChange;
 	GLfloat yChange;
+	GLfloat muevex;
+	GLfloat rotax, rotay, rotaz, articulacion1, articulacion2, articulacion3, articulacion4, articulacion5, articulacion6;
+	GLfloat anguloCofre = 0.0f;
+	GLfloat anguloLlantas = 0.0f;
+	GLfloat positionZ = 0.0f;
+	GLfloat positionX = 0.0f;
+	GLfloat posX = 0.0f;
+	GLfloat posY = 0.0f;
+	GLfloat prendeluzspot = 0.0f;
+	GLfloat cambialuzspot = 0.0f;
+	GLfloat prendeluzspot2 = 0.0f;
+	GLfloat cambialuzspot2 = 0.0f;
+	GLfloat prendeluzspot3 = 0.0f;
+	bool cambialuzfaros = true; 
+	bool isKPressed = false;
+	bool isLPressed = false;
+	// Variables para el movimiento hacia adelante y hacia atrás
+	bool isOPressed = false;
+	bool isPPressed = false;
 	bool mouseFirstMoved;
-	void createCallbacks();
+	//Variable para validar el cambio de camara
+	GLfloat valorCamara = 0.0f;
+	//mas
+	GLfloat screenX, screenY;
 	static void ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode);
 	static void ManejaMouse(GLFWwindow* window, double xPos, double yPos);
+	
 };
 
