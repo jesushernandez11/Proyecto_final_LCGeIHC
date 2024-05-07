f1c6261e24d92986b7008e49b6e24fce6f4c7a3e
vick-genis <99092155+vick-genis@users.noreply.github.com>
2024-05-06 20:06:19 -0600

    Add files via upload

--- DirectionalLight.h
+++ DirectionalLight.h
@@ -6,16 +6,25 @@ class DirectionalLight :
 {
 public:
 	DirectionalLight();
-	DirectionalLight(GLfloat red, GLfloat green, GLfloat blue, 
+	DirectionalLight(GLfloat red, GLfloat green, GLfloat blue,
 					GLfloat aIntensity, GLfloat dIntensity,
-					GLfloat xDir, GLfloat yDir, GLfloat zDir);//Representa la dirección del sol
+					GLfloat xDir, GLfloat yDir, GLfloat zDir); //el vector de direcion nos va a servir para el ciclo de dia y de noche*/
+	/*DirectionalLight(GLuint shadowWidth, GLuint shadowHeight,
+		GLfloat red, GLfloat green, GLfloat blue,
+		GLfloat aIntensity, GLfloat dIntensity,
+		GLfloat xDir, GLfloat yDir, GLfloat zDir);*/
+
 
 	void UseLight(GLfloat ambientIntensityLocation, GLfloat ambientcolorLocation,
 		GLfloat diffuseIntensityLocation, GLfloat directionLocation);
-
+	void SetDirection(GLfloat xDir, GLfloat yDir, GLfloat zDir);
+	glm::mat4 calculateLightTransform();
+	void setAmbientDiffuse(glm::vec2 values);
+	void setDirection(glm::vec3 dir);
 	~DirectionalLight();
 
 private:
 	glm::vec3 direction;
+	
 };
 
